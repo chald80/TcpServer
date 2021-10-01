33402070f04cec09e96775d01b21a172882be599
chald80 <72771464+chald80@users.noreply.github.com>
2021-10-01 09:26:31 +0200

    update

--- TcpServer/Program.cs
+++ TcpServer/Program.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+using Opgave1;
 
 namespace Opgave5
 {
@@ -9,6 +13,7 @@ namespace Opgave5
     {
         static void Main()
         {
+            
             Console.WriteLine("This is the server");
             TcpListener listener = new TcpListener(IPAddress.Loopback, 2121);
             listener.Start();
@@ -21,25 +26,83 @@ namespace Opgave5
             }
         }
 
+        public static List<FootBallPlayer> FootBallPlayers = new List<FootBallPlayer>()
+        {
+            new FootBallPlayer(1, "Anders", 2000, 42)
+        };
+
+
+
+        
         private static void DoClient(TcpClient socket)
         {
             NetworkStream ns = socket.GetStream();
             StreamReader reader = new StreamReader(ns);
             StreamWriter writer = new StreamWriter(ns);
-            string message = reader.ReadLine();
-            Console.WriteLine("Server received: " + message);
-            writer.Write(message);
-            writer.Flush();
-            socket.Close();
+            while (true)
+            {
+                string message1 = reader.ReadLine();
+                string message2 = reader.ReadLine();
+                Console.WriteLine("Server received: " + message1);
+
+                switch (message1)
+                {
+                    case "HentAlle":
+
+                        foreach (FootBallPlayer FBP  in FootBallPlayers)
+                        {
+                            Console.WriteLine($"List Of Players {FBP.Id} {FBP.Name} {FBP.ShirtNumber} {FBP.Price}");
+                            writer.WriteLine($"List Of Players {FBP.Id} {FBP.Name} {FBP.ShirtNumber} {FBP.Price}");
+                        }
+                        
+                        writer.WriteLine("HentAlle");
+                        writer.WriteLine(message1); 
+                        writer.WriteLine(message2);
+                        writer.Flush();
+                        break;
+
+                    case "Hent":
+
+                        writer.WriteLine("Hent");
+                        writer.WriteLine(message1);
+                        writer.WriteLine(message2);
+                        writer.Flush();
+                        break;
+
+
+                    case "Gem":
+                        FootBallPlayer FromJson = JsonSerializer.Deserialize<FootBallPlayer>(message2);
+                        FootBallPlayers.Add(FromJson);
+                        Console.WriteLine("FootBallPlayer" );
+
+                        writer.WriteLine("gem");
+                        writer.WriteLine(message1);
+                        writer.WriteLine(message2);
+                        writer.Flush();
+                        break;
+
+                    case "Close":
+
+                        writer.WriteLine("closing Socket");
+                        writer.Flush();
+                        socket.Close();
+                        break;
+                
+                }
+
+                //writer.WriteLine(message1);
+                //writer.WriteLine(message2);
+                //writer.Flush();
+            }
         }
 
-        private static IPAddress GetIpAddressOfThisComputer()
-            // https://www.delftstack.com/howto/csharp/get-local-ip-address-in-csharp/
-        {
-            IPHostEntry ipEntry = Dns.GetHostEntry(Dns.GetHostName());
-            IPAddress ipAddress = ipEntry.AddressList[0];
-            Console.WriteLine("Your IP address: " + ipAddress);
-            return ipAddress;
-        }
+        //private static IPAddress GetIpAddressOfThisComputer()
+        //    // https://www.delftstack.com/howto/csharp/get-local-ip-address-in-csharp/
+        //{
+        //    IPHostEntry ipEntry = Dns.GetHostEntry(Dns.GetHostName());
+        //    IPAddress ipAddress = ipEntry.AddressList[0];
+        //    Console.WriteLine("Your IP address: " + ipAddress);
+        //    return ipAddress;
+        //}
     }
 }
